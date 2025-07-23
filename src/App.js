91ecffd95352e9794659358219a16de6399260b7
charan <charanramagiri2004@gmail.com>
2025-07-23 23:57:28 +0530

    files created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CourseTypeManager from './components/CourseTypeManager';
+import CourseManager from './components/CourseManager';
+import CourseOffering from './components/CourseOffering';
+import StudentRegistration from './components/StudentRegistration';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>🎓 Student Registration System</h1>
+      <CourseTypeManager />
+      <CourseManager />
+      <CourseOffering />
+      <StudentRegistration />
     </div>
   );
 }
