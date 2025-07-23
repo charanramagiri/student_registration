1d7fd3924656a23b63f308402ce9848a4e8c1a45
charan <charanramagiri2004@gmail.com>
2025-07-24 00:09:45 +0530

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CourseTypeManager from './components/CourseTypeManager';
 import CourseManager from './components/CourseManager';
 import CourseOffering from './components/CourseOffering';
 import StudentRegistration from './components/StudentRegistration';
 
 function App() {
+  const [courseTypes, setCourseTypes] = useState([]);
+  const [courses, setCourses] = useState([]);
+
   return (
     <div>
       <h1>🎓 Student Registration System</h1>
-      <CourseTypeManager />
-      <CourseManager />
-      <CourseOffering />
+
+      {/* Manage Course Types */}
+      <CourseTypeManager
+        courseTypes={courseTypes}
+        setCourseTypes={setCourseTypes}
+      />
+
+      {/* Manage Courses */}
+      <CourseManager courses={courses} setCourses={setCourses} />
+
+      {/* Manage Course Offerings */}
+      <CourseOffering courseTypes={courseTypes} courses={courses} />
+
+      {/* Student Registration */}
       <StudentRegistration />
     </div>
   );
